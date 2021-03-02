2d0d76d616a7a8722a602628e13ba02f1e19f44b
jaugustomonteiro <jamonteirolima@gmail.com>
2021-03-01 23:47:43 -0300

    Aula03-23m:30s

---
+++ src/controllers/UserController.ts
@@ -0,0 +1,27 @@
+import { Request, Response } from 'express';
+import { getCustomRepository } from 'typeorm';
+import { UsersRepository } from '../repositories/UserRepository';
+
+class UserController {
+  async create(request: Request, response: Response) {
+    const { name, email } = request.body;
+    const usersRepository = getCustomRepository(UsersRepository);
+
+    const userAlreadyExists = await usersRepository.findOne({ email });
+
+    if (userAlreadyExists) {
+      return response.status(400).json({ error: 'User aready exists' });
+    }
+
+    const user = usersRepository.create({
+      name: name,
+      email: email,
+    });
+
+    await usersRepository.save(user);
+
+    return response.json(user);
+  }
+}
+
+export { UserController };
